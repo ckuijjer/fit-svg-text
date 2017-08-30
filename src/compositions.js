26c8d990e3c36f6898be7cd856924cf87d7a8875
Casper Kuijjer <casper@kuijjer.com>
2017-08-30 22:08:00 +0200

    find the best zoom level

---
+++ src/compositions.js
@@ -0,0 +1,29 @@
+// Non performing implementation of https://en.wikipedia.org/wiki/Composition_(combinatorics)
+const compositions = list => {
+  const bits = list.length - 1;
+  const number_of_compositions = Math.pow(2, bits);
+
+  return new Array(number_of_compositions).fill(0).map((_, i) => {
+    const composition = i.toString(2).padStart(bits, '0').split('').reduce((
+      acc,
+      bit,
+      j
+    ) => {
+      const item = list[j + 1];
+
+      if (bit === '0') {
+        // add it to the last list
+        acc[acc.length - 1].push(item);
+      } else {
+        // add it as a new list
+        acc.push([item]);
+      }
+
+      return acc;
+    }, [[list[0]]]);
+
+    return composition;
+  });
+};
+
+export default compositions;
