a7e9b88254b28fa95f1761edc3e0359ff86a9e1d
Casper Kuijjer <casper@kuijjer.com>
2018-03-30 21:47:37 +0200

    minimal documentation

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import Resizable from 're-resizable'
+import ContainerDimensions from 'react-container-dimensions'
 
 import Example from '../../src'
 
 class Demo extends Component {
   render() {
     return (
-      <div>
-        <h1>fit-svg-text Demo</h1>
-        <div style={{ border: '1px solid #ccc', display: 'inline-block' }}>
-          <Example
-            width="200"
-            height="300"
-            text="This text will maximally fill the block"
-          />
-        </div>
+      <div
+        style={{
+          backgroundColor: '#eee',
+          minHeight: '100vh',
+          padding: 72,
+        }}
+      >
+        <Resizable
+          defaultSize={{
+            width: 300,
+            height: 200,
+          }}
+          style={{ margin: '0 auto' }}
+        >
+          <ContainerDimensions>
+            {({ width, height }) => (
+              <Example width={width} height={height} text="Fit SVG Text" />
+            )}
+          </ContainerDimensions>
+        </Resizable>
       </div>
     )
   }
