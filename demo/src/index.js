82d38532e22b71ac9b3c0ed15174098022f99ce8
Casper Kuijjer <casper@kuijjer.com>
2018-03-30 21:04:37 +0200

    demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,23 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
 import Example from '../../src'
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>fit-svg-text Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <h1>fit-svg-text Demo</h1>
+        <div style={{ border: '1px solid #ccc', display: 'inline-block' }}>
+          <Example
+            width="200"
+            height="300"
+            text="This text will maximally fill the block"
+          />
+        </div>
+      </div>
+    )
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import React, {Component} from 'react'
+import React from 'react'
+import FitSVGTextMeasure from './FitSVGTextMeasure'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
-}
+const FitSVGTextRect = ({
+  width,
+  height,
+  text,
+  backgroundColor = '#fff',
+  color = '#000',
+}) => (
+  <svg width={width} height={height}>
+    <g>
+      <rect x="0" y="0" width={width} height={height} fill={backgroundColor} />
+      <FitSVGTextMeasure
+        x="0"
+        y="0"
+        stroke={color}
+        dy="1em"
+        width={width}
+        height={height}
+        text={text}
+      />
+    </g>
+  </svg>
+)
+
+export default FitSVGTextRect
