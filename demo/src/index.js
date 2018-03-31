d3dae38491e84886225c92586a04cd3f10deb4a2
Casper Kuijjer <casper@kuijjer.com>
2018-03-31 11:10:12 +0200

    Demo has handles

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,33 +2,65 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Resizable from 're-resizable'
 import ContainerDimensions from 'react-container-dimensions'
+import styled from 'react-emotion'
 
-import Example from '../../src'
+import FitSVGText from '../../src'
+
+const Page = styled('div')({
+  backgroundColor: '#fbfbfb',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+})
+
+const ResizableHandle = styled('div')({
+  position: 'absolute',
+  left: 7,
+  top: 7,
+  width: 4,
+  height: 4,
+  border: '1px solid #aaa',
+  backgroundColor: '#fff',
+  boxShadow: '1px 1px 1px 0px rgba(153,153,153,0.5)',
+})
+
+const StyledResizable = styled(Resizable)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: `solid 1px #ccc`,
+  background: '#f0f0f0',
+  marginTop: 40,
+  fontFamily: 'Helvetica',
+})
 
 class Demo extends Component {
   render() {
     return (
-      <div
-        style={{
-          backgroundColor: '#eee',
-          minHeight: '100vh',
-          padding: 72,
-        }}
-      >
-        <Resizable
+      <Page>
+        <StyledResizable
           defaultSize={{
             width: 300,
             height: 200,
           }}
-          style={{ margin: '0 auto' }}
+          handleComponent={{
+            topLeft: ResizableHandle,
+            topRight: ResizableHandle,
+            bottomRight: ResizableHandle,
+            bottomLeft: ResizableHandle,
+          }}
         >
           <ContainerDimensions>
             {({ width, height }) => (
-              <Example width={width} height={height} text="Fit SVG Text" />
+              <FitSVGText
+                width={width - 2}
+                height={height - 2}
+                text="Fit SVG Text"
+              />
             )}
           </ContainerDimensions>
-        </Resizable>
-      </div>
+        </StyledResizable>
+      </Page>
     )
   }
 }
